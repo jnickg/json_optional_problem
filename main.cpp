1c4463f4e72e60e105e12508d824fdb4dcdcb776
Nick Giampietro <nickgiampietro@zeroeyes.com>
2025-05-13 03:11:58 +0900

    Update to use `develop`
    - Fixes the issue

--- main.cpp
+++ main.cpp
@@ -29,23 +29,34 @@ NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(baz, _bar);
 
 int main()
 {
-    nlohmann::json j = nlohmann::json::parse(R"({"_b": 37})");
-    bar b;
-    j.get_to(b);
-    std::cout << "b._b: " << b._b << std::endl;
+    try{
+        nlohmann::json j = nlohmann::json::parse(R"({"_b": 37})");
+        bar b;
+        j.get_to(b);
+        std::cout << "b._b: " << b._b << std::endl;
+    } catch (const nlohmann::json::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
     // Un-comment the code below to break the build.
 
-    baz bz;
-    nlohmann::json j2 = nlohmann::json::parse(R"({"_bar": {"_b": 1}})");
-    j.get_to(bz);
+    try {
+        baz bz;
+        nlohmann::json j2 = nlohmann::json::parse(R"({"_bar": {"_b": 1}})");
+        j2.get_to(bz);
+        std::cout << "bz._bar->_f: " << (bz._bar ? std::to_string(bz._bar->_b) : "null") << std::endl;
+    } catch (const nlohmann::json::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
-    std::cout << "bz._bar->_f: " << (bz._bar ? std::to_string(bz._bar->_b) : "null") << std::endl;
-
-    foo f;
-    nlohmann::json j3 = nlohmann::json::parse(R"({"_f": 1})");
-    j2.get_to(f);
-    std::cout << "f._f: " << (f._f ? std::to_string(f._f.value()) : "null") << std::endl;
+    try {
+        foo f;
+        nlohmann::json j3 = nlohmann::json::parse(R"({"_f": 1})");
+        j3.get_to(f);
+        std::cout << "f._f: " << (f._f ? std::to_string(f._f.value()) : "null") << std::endl;
+    } catch (const nlohmann::json::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
     return 0;
 }
